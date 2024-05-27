acbfb206907e13ae976f8430968a7938ca1ddc7e
CCEKER <146904462+CCEKER@users.noreply.github.com>
2024-05-27 16:18:10 +0300

    Initial commit

---
+++ [Flow] - Login/Presenter/LoginPresenter.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPresenter.swift
+//  IMDbApp
+//
+//  Created by Cagatay Ceker on 24.01.2024.
+//
+
+import Foundation
+
+protocol LoginPresenterProtocol  {
+    
+}
+
+final class LoginPresenter: LoginPresenterProtocol {
+    
+    weak var viewController: LoginViewControllerProtocol?
+}
